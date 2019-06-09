ab42369af35e4d87c19cb472789b31b3f77a6084
Dmitry Mazur <innownedagain@gmail.com>
2019-06-09 20:45:13 +0300

    внёс правки по тревису 2

--- js/stat.js
+++ js/stat.js
@@ -1,21 +1,22 @@
+'use strict';
+
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
 var FONT_GAP = 20;
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
-var TEXT_WIDTH = 50;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var GAP_MULTIPLIER = 2;
 var PLAYER_COLOR = 'rgba(255, 0, 0, 1)';
 
-renderCloud = function (ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-getMaxElement = function (arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -26,7 +27,7 @@ getMaxElement = function (arr) {
   return maxElement;
 };
 
-changeBarColor = function (playerName) {
+var changeBarColor = function (playerName) {
   var min = 0.10;
   var max = 1;
   var barColor = PLAYER_COLOR;
@@ -39,7 +40,7 @@ changeBarColor = function (playerName) {
   // return playerName === 'Вы' ? PLAYER_COLOR : `rgba(0, 0, 255, ${(Math.random() *(max - min) + min).toFixed(2)})`;
 
   return barColor;
-}
+};
 
 window.renderStatistics = function (ctx, players, times) {
   var maxTime = getMaxElement(times);
